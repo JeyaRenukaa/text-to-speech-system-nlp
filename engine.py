8e05371b1f86ef41f2bcf871c6dff9be8ac0cbe7
JeyaRenukaa <renukaaselvam05@gmail.com>
2025-05-20 21:52:40 +0530

    Create engine.py

---
+++ engine.py
@@ -0,0 +1,15 @@
+import pyttsx3
+
+
+
+def speak(text):
+
+    engine = pyttsx3.init()
+
+    engine.setProperty('rate', 150)
+
+    engine.setProperty('volume', 0.9)
+
+    engine.say(text)
+
+    engine.runAndWait()
